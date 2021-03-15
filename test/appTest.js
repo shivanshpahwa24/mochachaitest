15a10b301854e25e1b2d9eee129fe07ab76c45b0
Shivansh <shivanshpahwa24@gmail.com>
2021-03-16 01:17:26 +0530

    Finalizing

--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,35 @@
 const assert = require("chai").assert;
+//const sayHello = require("../app").sayHello;
+//const addNumbers = require("../app").addNumbers;
+
 const app = require("../app");
 
+//Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5, 5);
+
 describe("App", function () {
-  it("app should return hello", function () {
-    assert.equal(app(), "hello");
+  describe("sayHello()", function () {
+    it("sayHello should return hello", function () {
+      let result = sayHelloResult;
+      assert.equal(result, "hello");
+    });
+
+    it("sayHello should return type string", function () {
+      let result = sayHelloResult;
+      assert.typeOf(result, "string");
+    });
+  });
+
+  describe("addNumbers", function () {
+    it("addNumbers should return sum above 5", function () {
+      let result = addNumbersResult;
+      assert.isAbove(result, 5);
+    });
+
+    it("addNumbers should return type number", function () {
+      let result = addNumbersResult;
+      assert.typeOf(result, "number");
+    });
   });
 });
